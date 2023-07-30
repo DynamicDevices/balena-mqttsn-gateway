787cb231382dad869fe0b46f918f1e102453b9fa
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2023-07-30 15:15:51 +0100

    run-mqtt-sngateway: Fix typo
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- mqttsn-gateway/run-mqtt-sngateway.sh
+++ mqttsn-gateway/run-mqtt-sngateway.sh
@@ -6,7 +6,7 @@ sed -i "s/ENV_BROKER_SECURE_PORT/${BROKER_SECURE_PORT}/" gateway.conf
 sed -i "s/ENV_GATEWAY_PORT/${GATEWAY_PORT}/" gateway.conf
 sed -i "s/ENV_GATEWAY_IPV6_PORT/${GATEWAY_IPV6_PORT}/" gateway.conf
 sed -i "s/ENV_MULTICAST_IPV6_IF/${MULTICAST_IPV6_IF}/" gateway.conf
-sed -i "s/ENV_MULTICAST_HOPS/${MULTICAST_HOPS}/" gateway.conf
+sed -i "s/ENV_MULTICAST_HOPS/${MULTICAST_IPV6_HOPS}/" gateway.conf
 
 export LD_LIBRARY_PATH=.
 
